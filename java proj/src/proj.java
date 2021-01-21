00d23a55af4e110f0d8c12f101e791c9383dba70
MUBHASHIRAWIN10\mubhashira <sayedmubashira@mindfiresolutions.com>
2021-01-21 12:49:54 +0530

    init commit message

---
+++ java proj/src/proj.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author mubhashira
+ *
+ */
+public class proj {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello this is a PRINTING Demo Program");
+
+	}
+
+}
